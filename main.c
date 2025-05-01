bdaceabfb49da6742502ca00cee31211648205be
Hlebotron <stabasov@gmail.com>
2025-05-01 14:55:17 +0200

    Fix all segfaults

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <stdint.h>
 #include <math.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 450;
@@ -33,12 +34,12 @@ Segment segment_from_points(Vector2 start, Vector2 end) {
     return segment;
 }
 Line line_from_points(
-    Vector2 point1,
-    Vector2 point2
+    Vector2* point1,
+    Vector2* point2
 ) {
-    int A = point1.y - point2.y;
-    int B = point2.x - point1.x;
-    int C = A * point1.x + B * point1.y;
+    int A = point1->y - point2->y;
+    int B = point2->x - point1->x;
+    int C = A * point1->x + B * point1->y;
     Line line = {
 	A,
 	B,
@@ -48,14 +49,13 @@ Line line_from_points(
     return line; 
 }
 Line line_from_angle(
-    float angle_deg, //Positive is clockwise
-    float p_x, 
-    float p_y
+    Vector2* point,
+    float angle_deg //Positive is clockwise
 ) {
     float math_angle = -angle_deg;
     float A = sin(angle_deg / 180 * M_PI);
     float B = cos(angle_deg / 180 * M_PI);
-    float C = A * p_x + B * p_y;
+    float C = A * point->x + B * point->y;
     Line line = {
 	A,
 	B,
@@ -67,50 +67,63 @@ Line line_from_angle(
 
 
 
-Vector2* intersect_lines(
-    Line line1,
-    Line line2
+void intersect_lines(
+    Line* line1,
+    Line* line2,
+    Vector2* intersect
 ) {
-    float denom = (line1.A - (line1.B * line2.A / line2.B));
-    if (denom == 0) return NULL;
-    float x = (line1.C - (line1.B * line2.C / line2.B)) / denom;
-    float y = (line2.C - (line2.A - x)) / line2.B;
-    Vector2* ptr = malloc(sizeof(Vector2));
-    if (ptr == NULL) {
-	printf("Allocation of Vector2 failed");
-	exit(0);
+    float denom = (line1->A - (line1->B * line2->A / line2->B));
+    if (denom == 0) { 
+	intersect = NULL;
+	return;
     }
-    Vector2 vector = {
-	x,
-	y
-    };
-    *ptr = vector;
-    return ptr;
+    float x = (line1->C - (line1->B * line2->C / line2->B)) / denom;
+    float y = (line2->C - (line2->A - x)) / line2->B;
+    *intersect = (Vector2){ x, y };
 }
-Vector2* intersect_segment(
-    Segment segment,
-    Line line
+bool intersect_segment(
+    Segment* segment,
+    Line* line,
+    Vector2* intersect
 ) {
-    Line seg_line = line_from_points(segment.start, segment.end);
-    Vector2* intersect = intersect_lines(seg_line, line);
+    Line seg_line = line_from_points(&segment->start, &segment->end);
+    intersect_lines(&seg_line, line, intersect);
     if (
 	intersect == NULL ||
-	intersect->x < segment.start.x ||
-	intersect->x > segment.end.x
-    ) return NULL; 
-    else return intersect; 
+	intersect->x < segment->start.x ||
+	intersect->x > segment->end.x
+    ) {
+	intersect = NULL;
+	return false;
+    };
+    return true;
 }
-float distance(
-    Vector2 point1,
-    Vector2 point2
+float hor_distance(
+    Vector2* point1,
+    Vector2* point2
 ) { 
-    return sqrt(pow((point2.x - point1.x), 2) + pow((point2.y - point1.y), 2));
+    return sqrt(pow((point2->x - point1->x), 2) + pow((point2->y - point1->y), 2));
 };
-float displayed_height_deg(
-    Vector2 point1,
-    Vector2 point2
+void displayed_height_deg (
+    Vector2* point1,
+    Vector2* point2,
+    float obj_height,
+    float* bottom,
+    float* top
 ) {
-    float dist = distance(point1, point2);
+    float dist = hor_distance(point1, point2);
+    if (dist == 0) {
+	*top = VERTICAL_FOV / 2;	
+	*bottom = VERTICAL_FOV / 2;	
+	return;
+    }
+    printf("Distance: %f\n", dist);
+    printf("Height: %f\n", (obj_height - PLAYER_HEIGHT));
+    float val1 = atan((obj_height - PLAYER_HEIGHT) / dist) / M_PI * 180;
+    float val2 = atan(PLAYER_HEIGHT / dist) / M_PI * 180;
+    printf("%f %f\n", val1, val2);
+    *top = atan((obj_height - PLAYER_HEIGHT) / dist) / M_PI * 180;
+    *bottom = atan(PLAYER_HEIGHT / dist) / M_PI * 180;
 };
 
 int main() {
@@ -118,14 +131,26 @@ int main() {
 	(Vector2){ 0, 1 }, 
 	(Vector2){ 2, 2 } 
     };
-    Line orient = line_from_angle(125, 0, 0);
+    Player player = {
+	(Vector2){ 0, 0 },
+	125
+    };
     InitWindow(WIDTH, HEIGHT, "Gloom");
     while(!WindowShouldClose()) {
-	Vector2* intersect = intersect_segment(wall, orient);
+	printf("");
+	Line orient = line_from_angle(&player.pos, player.orient);
+	Vector2 intersect;
+	bool res = intersect_segment(&wall, &orient, &intersect);
+	if (res) {
+	    float bottom, top;
+	    displayed_height_deg(&player.pos, &intersect, 3, &bottom, &top);
+	};
 	ClearBackground(RAYWHITE);
 	BeginDrawing();
 	EndDrawing();
-	free(intersect);
+	//free(intersect);
+	usleep(100000);
+	//printf("pog");
     }
     CloseWindow();
 }
